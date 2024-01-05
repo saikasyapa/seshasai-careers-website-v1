d4d04c3862869f615c9fbcc45d6f5cc1d432a10f
saikasyapa <146060560+saikasyapa@users.noreply.github.com>
2024-01-05 20:36:12 +0000

    some modifications

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-  from flask import Flask, render_template, jsonify,request
-  from database import load_jobs_from_db,load_job_from_db,add_applications_to_db
+from flask import Flask, render_template, jsonify, request
 
-  app = Flask(__name__)
+from database import load_jobs_from_db, load_job_from_db, add_applications_to_db
 
-
-
+app = Flask(__name__)
 
 
-  @app.route("/")
-  def hello_world():
-    jobs = load_jobs_from_db()
-    return render_template("home.html",
-                           jobs=jobs)
+@app.route("/")
+def hello_world():
+  jobs = load_jobs_from_db()
+  return render_template("home.html", jobs=jobs)
 
 
-  @app.route("/api/jobs")
-  def list_jobs():
-    jobs = load_jobs_from_db()
-    return jsonify(jobs)
-
-  @app.route("/job/<id>")
-  def show_job(id):
-    job=load_job_from_db(id)
-    return render_template("jobpage.html",job=job)
+@app.route("/api/jobs")
+def list_jobs():
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 
-  @app.route("/job/<id>/apply",methods=['post'])
-  def apply_to_job(id):
-    data=request.form
-    job=load_job_from_db(id)
-    add_applications_to_db(id,data)
-    return render_template('application_submitted.html',application=data,job=job)
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  return render_template("jobpage.html", job=job)
 
-  if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_applications_to_db(id, data)
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)

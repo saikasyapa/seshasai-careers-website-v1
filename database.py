54d6a3c15dc7b3771b92c97e599ca45caa092c05
saikasyapa <146060560+saikasyapa@users.noreply.github.com>
2024-01-05 20:11:48 +0000

    Data is stored in database

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import mysql.connector
-# import os
+#import os
 
 # db_connection_string = os.environ['DB_CONNECTION_STRING']
 
@@ -42,5 +42,72 @@ def load_job_from_db(id):
 
   # Close the cursor
   mycursor.close()
-
   return row
+
+
+import mysql.connector
+
+
+def add_applications_to_db(job_id, data):
+
+  cursor = mydb.cursor()
+
+  add_application = (
+      "INSERT INTO auroraconsulting.applications "
+      "(job_id, full_name, email, linkedin_url, education, work_experience, resume_url) "
+      "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+
+  application_data = (job_id, data['full_name'], data['email'],
+                      data['linkedin_url'], data['education'],
+                      data['work_experience'], data['resume_url'])
+
+  cursor.execute(add_application, application_data)
+
+  mydb.commit()
+
+  cursor.close()
+  mydb.close()
+
+
+# mycursor = mydb.cursor(dictionary=True)
+# def add_applications_to_db(job_id,full_name):
+
+# # Assuming you have a table named 'your_table' with columns 'column1', 'column2', and 'column3'
+# # You will receive the data from the user and then insert it into the table
+
+# # SQL query to insert data into the table
+#  insert_query = "INSERT INTO auroraconsulting.applications (job_id, full_name) VALUES (%s, %s)"
+
+# # Data to be inserted into the table
+#  values = (job_id, full_name)
+
+# # Execute the query
+#  mycursor.execute(insert_query, values)
+
+# # Commit changes to the database
+#  mydb.commit()
+
+# # Close the cursor and database connection
+#  mycursor.close()
+#  mydb.close()
+
+# def add_applications_to_db(job_id, application):
+#     mycursor = mydb.cursor(dictionary=True)
+#     add_application = ("INSERT INTO auroraconsulting.applications "
+#                        "(job_id, full_name, email, linkedin_url, education, experience, resume_url) "
+#                        "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+
+#     application_data = (
+#         application['job_id'],
+#         application['full_name'],
+#         application['email'],
+#         application['linkedin_url'],
+#         application['education'],
+#         application['experience'],
+#         application['resume_url']
+#     )
+
+#     mycursor.execute(add_application, application_data)
+#     mydb.commit()
+#     mycursor.close()
+#     mydb.close()
